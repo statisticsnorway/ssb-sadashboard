b8e1808a263597cec637a997ab6ac64a03c3ac13
ffe811 <ffe@ssb.no>
2025-04-11 09:36:19 +0000

    Added data

--- R/data.R
+++ R/data.R
@@ -1,21 +1,25 @@
 #' Norwegian Retail Indexes
 #'
-#' A time series object containing various Norwegian retail indexes from January 2000 to January 2025. Hentet fra statistikkbanken 07.04.25
+#' A time series object containing Norwegian Retail Indexes from January 2000 to February 2025.
+#'
+#' Statistisk sentralbyrå. (2025, 11. April). 07129: Varehandelsindeksen, etter næring, måned og statistikkvariabel. Grunnlagstall (K) 2008 - 2022 [Statistikk].
+#'  https://data.ssb.no/api/v0/no/table/07129/
+#' Hentet fra statistikkbanken 07.04.25
 #'
 #'
-#' @format A time series object with some rows and some variables:
-#' \describe{
-#'   \item{47}{quality of the cut (Fair, Good, Very Good, Premium, Ideal)}
-#' }
+#' @format A time series object where each column contains Retail index for NACE 47
+#'
 "vhi"
 
 
 #' Ledige Stillinger
 #'
-#' A time series object containing various Norwegian vacant positions from K1 2010 to K1 2025. Hentet fra statistikkbanken 07.04.25
+#' A time series object containing various Norwegian vacant positions from K1 2010 to K1 2025.
+#'
+#' Hentet fra statistikkbanken 11.04.25
 #'
 #'
-#' @format A time series object with 60 rows and 51 variables:
+#' @format A time series object
 #' \describe{
 #'   \item{..}{noe }
 #' }
