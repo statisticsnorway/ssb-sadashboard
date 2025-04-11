121cbf701ed50f2070d73b48fd8b4d58ed415b3b
ffe811 <ffe@ssb.no>
2025-04-11 12:28:31 +0000

    beskrivelse av data

--- R/data.R
+++ R/data.R
@@ -1,26 +1,24 @@
 #' Norwegian Retail Indexes
 #'
-#' A time series object containing Norwegian Retail Indexes from January 2000 to February 2025.
-#'
-#' Statistisk sentralbyrå. (2025, 11. April). 07129: Varehandelsindeksen, etter næring, måned og statistikkvariabel. Grunnlagstall (K) 2008 - 2022 [Statistikk].
-#'  https://data.ssb.no/api/v0/no/table/07129/
-#' Hentet fra statistikkbanken 07.04.25
+#' A time series object with unadjusted monthly Norwegian Retail Indexes from January 2000 to February 2025.
 #'
 #'
-#' @format A time series object where each column contains Retail index for NACE 47
 #'
+#' @format R time series object with 35 columns and 302 rows.
+#' Columns contains various Norwegian unadjusted value indexes in NACE group 47, retail trade, except of motor vehicles and motorcycles.
+#' Indices are produced monthly by Statistics Norway.
+#'
+#' @source From Statbank Norway. (April 11, 2025), https://www.ssb.no/en/statbank/table/07129
 "vhi"
 
 
-#' Ledige Stillinger
+#' Job Vacanicies
 #'
-#' A time series object containing various Norwegian vacant positions from K1 2010 to K1 2025.
+#' A time series object with unadjusted quarterly Norwegian job vacancy rates. Indexes from first quarter 2010 to fourth quarter 2024.
 #'
-#' Hentet fra statistikkbanken 11.04.25
+#' @format R time series object with 21 columns and 60 rows.
+#' Columns contains various indexes for Norwegian job vacancy rates in various major industries (SIC2007).
+#' Indices are produced quarterly by Statistics Norway.
 #'
-#'
-#' @format A time series object
-#' \describe{
-#'   \item{..}{noe }
-#' }
-"ledige"
+#'@source From Statbank Norway. (April 11, 2025), https://www.ssb.no/en/statbank/table/08771/
+#'"ledige"
