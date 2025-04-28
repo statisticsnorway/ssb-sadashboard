e0fb1ed364dc3bced9e1d28be927640044db8a92
ffe811 <ffe@ssb.no>
2025-04-28 12:02:35 +0000

    beskrivelse data, utvidet tester

--- tests/testthat/test-make_plot_fig.R
+++ tests/testthat/test-make_plot_fig.R
@@ -1,3 +1,22 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("make_plot_fig returns a list of plotly objects", {
+
+  data_monthly <- sadashboard::vhi[,"47.3"]
+  spec_wd <- RJDemetra::x13_spec("RSA3",tradingdays.option="WorkingDays")
+  spec_td <- RJDemetra::x13_spec("RSA3",tradingdays.option="TradingDays")
+
+  model_1 <-  RJDemetra::x13(data_monthly,spec=spec_wd)
+  model_2 <-  RJDemetra::x13(data_monthly,spec=spec_td)
+
+  models_in <- list(series1=model_1,series2=model_2)
+  series_now <- c("series1", "series2")
+  result <- make_plot_fig(models_in, series_now)
+  expect_true(is.list(result))
+  expect_true(all(sapply(result, inherits, "plotly")))
+
+  result <- make_plot_fig(models_in, series_now, plot_start = NULL)
+  expect_true(is.list(result))
+
 })
+
+
--- tests/testthat/test-sa_quality_report.R
+++ tests/testthat/test-sa_quality_report.R
@@ -1,3 +1,16 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+
+test_that("sa_quality_report generates a report file", {
+  data_monthly <- sadashboard::vhi[,"47.3"]
+  spec_wd <- RJDemetra::x13_spec("RSA3",tradingdays.option="WorkingDays")
+  spec_td <- RJDemetra::x13_spec("RSA3",tradingdays.option="TradingDays")
+
+  model_1 <-  RJDemetra::x13(data_monthly,spec=spec_wd)
+  model_2 <-  RJDemetra::x13(data_monthly,spec=spec_td)
+
+  models_in <- list(series1=model_1,series2=model_2)
+  series_now <- c("series1", "series2")
+
+  report_file <- tempfile(fileext = ".html")
+  sa_quality_report(models_in, report_file)
+  expect_true(file.exists(report_file))
 })
