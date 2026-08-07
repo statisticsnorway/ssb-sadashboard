8b2a46c66798261ca511d8a76068dc002d6e3536
ffe811 <jens.haug@ssb.no>
2026-08-07 12:03:51 +0200

    branch for td_fix

--- R/td_p.R
+++ R/td_p.R
@@ -17,23 +17,27 @@
 
 td_p <- function(model_now){
 
-  if(TRUE %in% (rownames(model_now$regarima$regression.coefficients) %in% c("td15","td16","Week days","hverdag","WorkingDays"))){
-    here_now <- which(rownames(model_now$regarima$regression.coefficients) %in% c("td15","td16", "Week days","hverdag","WorkingDays"))
-    td_p_now <- tryCatch({stats::pt(abs(model_now$regarima$regression.coefficients[here_now,3]),
-                                    model_now$regarima[[5]][3]-model_now$regarima[[5]][2],lower.tail=FALSE)},error=function(e){-99})
-  }else if(TRUE %in% (rownames(model_now$regarima$regression.coefficients) %in% c("man","Monday"))){
-    here_now <- which(rownames(model_now$regarima$regression.coefficients) %in% c("man","tir","ons","tor","fre","lor",
-                                                                                  "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"))
-    td_p_now <- tryCatch({car::linearHypothesis(model_now,rownames(model_now$regarima$regression.coefficients)[here_now],
-                                                rep(0,length(here_now)),test="F")[2,4]},error=function(e){-99})
-  }else if(TRUE %in% (rownames(model_now$regarima$regression.coefficients) %in% "gr_1")){
-    here_now <- which(rownames(model_now$regarima$regression.coefficients) %in% paste0("gr_",1:6))
-    td_p_now <- tryCatch({car::linearHypothesis(model_now,rownames(model_now$regarima$regression.coefficients)[here_now],
-                                                rep(0,length(here_now)),test="F")[2,4]},error=function(e){-99})
+  if(requireNamespace("car",quietly=TRUE)){
+
+    if(TRUE %in% (rownames(model_now$regarima$regression.coefficients) %in% c("td15","td16","Week days","hverdag","WorkingDays"))){
+      here_now <- which(rownames(model_now$regarima$regression.coefficients) %in% c("td15","td16", "Week days","hverdag","WorkingDays"))
+      td_p_now <- tryCatch({stats::pt(abs(model_now$regarima$regression.coefficients[here_now,3]),
+                                      model_now$regarima[[5]][3]-model_now$regarima[[5]][2],lower.tail=FALSE)},error=function(e){-99})
+    }else if(TRUE %in% (rownames(model_now$regarima$regression.coefficients) %in% c("man","Monday"))){
+      here_now <- which(rownames(model_now$regarima$regression.coefficients) %in% c("man","tir","ons","tor","fre","lor",
+                                                                                    "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"))
+      td_p_now <- tryCatch({car::linearHypothesis(model_now,rownames(model_now$regarima$regression.coefficients)[here_now],
+                                                  rep(0,length(here_now)),test="F")[2,4]},error=function(e){-99})
+    }else if(TRUE %in% (rownames(model_now$regarima$regression.coefficients) %in% "gr_1")){
+      here_now <- which(rownames(model_now$regarima$regression.coefficients) %in% paste0("gr_",1:6))
+      td_p_now <- tryCatch({car::linearHypothesis(model_now,rownames(model_now$regarima$regression.coefficients)[here_now],
+                                                  rep(0,length(here_now)),test="F")[2,4]},error=function(e){-99})
+    }else{
+      td_p_now <- NA
+    }
   }else{
     td_p_now <- NA
   }
-
   return(td_p_now)
 }
 
